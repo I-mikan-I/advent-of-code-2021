720ef9fffddd38dacd705d27ac5865619b7d07d8
mikan <67881102+I-mikan-I@users.noreply.github.com>
2021-12-21 09:06:57 +0100

    Implement Solution to day 20 & 21

---
+++ 2021/day-20/src/main.rs
@@ -0,0 +1,94 @@
+use std::path::Path;
+
+
+fn main() {
+    let path = if let Some(x) = std::env::args().nth(1) {
+        x
+    } else {
+        "2021/day-20/input".to_string()
+    };
+    let path = Path::new(&path);
+    let contents = std::fs::read_to_string(path).unwrap();
+    let res_1 = challenge(&contents, 2);
+    let res_2 = challenge(&contents, 50);
+    println!("Solution to part 1: {}!\n\
+    Solution to part 2: {}!", res_1, res_2);
+}
+
+fn generate_row(k: usize, row: &[u8], rim: u8) -> [u8; 3] {
+    match k {
+        k if k >= 1 && k <= row.len() - 2 => row[k - 1..=k + 1].try_into().unwrap(),
+        k if k >= 1 => [row[k - 1], row[k], rim],
+        k if k <= row.len() - 2 => [rim, row[k], row[k + 1]],
+        _ => panic!()
+    }
+}
+
+fn challenge(input: &str, iterations: usize) -> usize {
+    let mut lines = input.lines().peekable();
+    let decoder: Vec<u8> = lines
+        .next()
+        .unwrap()
+        .chars()
+        .map(|char| match char {
+            '.' => 0,
+            '#' => 1,
+            _ => panic!(),
+        })
+        .collect::<Vec<_>>();
+    lines.next();
+    let cols = lines.peek().unwrap().len();
+    let mut image = Vec::with_capacity(iterations * 2);
+    image.resize(iterations, vec![0; iterations * 2 + cols]);
+    for (i, line) in lines.enumerate() {
+        image.push(vec![0; iterations * 2 + cols]);
+        for (k, v) in line.chars().enumerate() {
+            image[i+iterations][k+iterations] = match v {
+                '.' => 0,
+                '#' => 1,
+                _ => panic!(),
+            };
+        }
+    }
+    image.resize(image.len() + iterations, vec![0; iterations * 2 + cols]);
+    let mut image_new = image.clone();
+    let mut rim = 0;
+    let (mut i, mut k);
+    let (mut ib, mut kb) = (image.len() - iterations, image[0].len() - iterations);
+    for n in 1..=iterations {
+        let begin = iterations - n;
+        image[begin].fill(rim);
+        image[ib].fill(rim);
+        i = begin;
+        k = i;
+        for row in &mut image {
+            row[k] = rim;
+            row[kb] = rim;
+        }
+        while i <= ib {
+            while k <= kb {
+                let row1: [u8; 3] = generate_row(k + n - iterations, &image[i][begin..kb + 1], rim);
+                let row0 = if i > begin { generate_row(k + n - iterations, &image[i - 1][begin..kb + 1], rim) } else { [rim, rim, rim] };
+                let row2 = if i < image.len() - 1 - begin { generate_row(k + n - iterations, &image[i + 1][begin..kb + 1], rim) } else { [rim, rim, rim] };
+                let index = generate_index([row0, row1, row2].into_iter().flatten());
+                image_new[i][k] = decoder[index];
+                k += 1;
+            }
+            k = iterations - n;
+            i += 1;
+        }
+        ib += 1;
+        kb += 1;
+        rim = decoder[generate_index([rim; 9].into_iter())];
+        std::mem::swap(&mut image, &mut image_new);
+    }
+    image.into_iter().flatten().map(|byte| byte as usize).sum::<usize>()
+}
+
+fn generate_index(iter: impl Iterator<Item=u8>) -> usize {
+    iter.fold(0_usize, |agg, new| {
+        let mut agg = agg << 1;
+        agg += new as usize;
+        agg
+    })
+}
---
+++ 2021/day-21/src/main.rs
@@ -0,0 +1,126 @@
+use itertools::Itertools;
+use std::cmp::{max, min};
+use std::str::FromStr;
+
+fn main() {
+    let p1 = std::env::args()
+        .nth(1)
+        .map(|s| usize::from_str(&s).ok())
+        .flatten()
+        .expect("Please provide player 1's position as the first argument");
+    let p2 = std::env::args()
+        .nth(2)
+        .map(|s| usize::from_str(&s).ok())
+        .flatten()
+        .expect("Please provide player 2's position as the first argument");
+    let res_01 = ex_01(p1, p2);
+    let res_02 = ex_02(p1, p2);
+    println!(
+        "Solution to part 1: {}!\n\
+    Solution to part 2: {}!",
+        res_01, res_02
+    );
+}
+
+struct Die(usize);
+
+impl Die {
+    fn new() -> Die {
+        Die(0)
+    }
+    fn roll(&mut self) -> usize {
+        self.0 += 1;
+        self.0
+    }
+}
+
+fn ex_01(mut player1: usize, mut player2: usize) -> usize {
+    const BOARD_SIZE: usize = 10;
+    const GOAL: usize = 1000;
+    let mut die = Die::new();
+    let mut p1_score = 0;
+    let mut p2_score = 0;
+
+    let mut rotation = true;
+    while p1_score < GOAL && p2_score < GOAL {
+        let (player, score) = if rotation {
+            (&mut player1, &mut p1_score)
+        } else {
+            (&mut player2, &mut p2_score)
+        };
+        let rolls = std::iter::repeat_with(|| die.roll()).take(3).sum::<usize>();
+        *player = (*player - 1 + rolls) % BOARD_SIZE + 1;
+        *score += *player;
+        rotation = !rotation;
+    }
+    min(p1_score, p2_score) * die.0
+}
+
+fn ex_02(player1: usize, player2: usize) -> usize {
+    const BOARD_SIZE: usize = 10;
+    const GOAL: usize = 21;
+    let mut p1_wins = 0_usize;
+    let mut p2_wins = 0_usize;
+
+    let mut scores = [[[[0; BOARD_SIZE]; BOARD_SIZE]; GOAL]; GOAL];
+    scores[0][0][player1 - 1][player2 - 1] = 1;
+
+    let mut rotation = true;
+    let rolls: [usize; 27] = (1..=3)
+        .cartesian_product(1..=3)
+        .cartesian_product(1..=3)
+        .map(|((f, s), t)| f + s + t)
+        .collect::<Vec<_>>()
+        .try_into()
+        .unwrap();
+    loop {
+        let mut scores_t = scores;
+        for i in 0..GOAL {
+            for k in 0..GOAL {
+                if rotation {
+                    for j in 0..BOARD_SIZE {
+                        let new_positions = rolls
+                            .iter()
+                            .map(|roll| (j + roll) % BOARD_SIZE)
+                            .collect::<Vec<_>>();
+                        for l in 0..BOARD_SIZE {
+                            for &np in &new_positions {
+                                let new_score = i + np + 1;
+                                if new_score >= GOAL {
+                                    p1_wins += scores[i][k][j][l];
+                                } else {
+                                    scores_t[new_score][k][np][l] += scores[i][k][j][l];
+                                }
+                            }
+                            scores_t[i][k][j][l] -= scores[i][k][j][l];
+                        }
+                    }
+                } else {
+                    for l in 0..BOARD_SIZE {
+                        let new_positions = rolls
+                            .iter()
+                            .map(|roll| (l + roll) % BOARD_SIZE)
+                            .collect::<Vec<_>>();
+                        for j in 0..BOARD_SIZE {
+                            for &np in &new_positions {
+                                let new_score = k + np + 1;
+                                if new_score >= GOAL {
+                                    p2_wins += scores[i][k][j][l];
+                                } else {
+                                    scores_t[i][new_score][j][np] += scores[i][k][j][l];
+                                }
+                            }
+                            scores_t[i][k][j][l] -= scores[i][k][j][l];
+                        }
+                    }
+                }
+            }
+        }
+        rotation = !rotation;
+        if scores_t == scores {
+            break;
+        }
+        scores = scores_t;
+    }
+    max(p1_wins, p2_wins)
+}
